39ba41eb88907570899e3d596c6747bbe859fa4e
Byron Ferguson <byron.ferguson@homecreditus.com>
2017-08-17 02:33:13 -0500

    Added Hours, FAQ

--- pages/hours/index.vue
+++ pages/hours/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <h1>Hours</h1>
+<div>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1 has-text-centered hero-title">Hours</h1>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <article class="container">
+      <div class="box has-text-centered">
+    
+        <h1 class="title is-3">Monday - Saturday: 8am - 5pm</h1>
+        <h1 class="subtitle is-3">Phones answered 7am - 9pm</h1><br/>
+        
+        <h1 class="title is-4">Deliveries</h1>
+        <h1 class="subtitle is-4">
+          Evening & Sunday deliveries also 
+        </h1>
+
+    
+      </div>
+    </article>
+  </section>
+
+
+
+</div>
 </template>
 
 <script>
